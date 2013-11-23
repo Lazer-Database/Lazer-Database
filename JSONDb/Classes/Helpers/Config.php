8f4cb85ffa5c725d95f8ed875d80b0e4587bc930
Gregory <grego_92@o2.pl>
2013-11-23 22:52:20 +0100

    Renamed and changed whole namespaces to PSR-0. Changed logic of work

---
+++ JSONDb/Classes/Helpers/Config.php
@@ -0,0 +1,81 @@
+<?php
+
+ namespace JSONDb\Classes\Helpers;
+
+defined('JSONDB_SECURE') or die('Permission denied!');
+
+ /**
+  * Config managing class
+  *
+  * @category Helpers
+  * @author Grzegorz Kuźnik
+  * @copyright (c) 2013, Grzegorz Kuźnik
+  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+  */
+ class Config extends File {
+
+     public static function name($name)
+     {
+         $file = new Config;
+         $file->_name = $name;
+         $file->setType('config');
+
+         return $file;
+     }
+
+     /**
+      * Get key from returned config
+      * @param type $field key
+      * @param type $assoc
+      * @return mixed
+      */
+     public function getKey($field, $assoc=false)
+     {
+         return $assoc ? $this->get($assoc)[$field] : $this->get($assoc)->{$field};
+     }
+     
+     /**
+      * Return array with names of fields
+      * @return array
+      */
+     public function fields()
+     {
+         return array_keys($this->getKey('schema', true));
+     }
+
+     /**
+      * Return relations configure
+      * @return array|object
+      */
+     public function relations($table = null, $assoc = false)
+     {
+         if ($table !== null)
+         {
+             return $this->getKey('relations', $assoc)->{$table};
+         }
+
+         return $this->getKey('relations', $assoc);
+     }
+
+     /**
+      * Returning assoc array with types of fields
+      * @return array
+      */
+     public function schema()
+     {
+         return $this->getKey('schema', true);
+     }
+
+     /**
+      * Returning last ID from table
+      * @param string $name
+      * @return integer
+      */
+     public function last_id()
+     {
+         return $this->getKey('last_id');
+     }
+
+ }
+
+?>
---
+++ JSONDb/Classes/Helpers/File.php
@@ -0,0 +1,108 @@
+<?php
+
+ namespace JSONDb\Classes\Helpers;
+
+use JSONDb\Classes\Exception;
+
+defined('JSONDB_SECURE') or die('Permission denied!');
+
+ /**
+  * File managing class
+  *
+  * @category Helpers
+  * @author Grzegorz Kuźnik
+  * @copyright (c) 2013, Grzegorz Kuźnik
+  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+  */
+ class File implements FileInterface {
+
+     /**
+      * File name
+      * @var string
+      */
+     protected $_name;
+
+     /**
+      * File type (data|config)
+      * @var string
+      */
+     protected $_type;
+
+     
+     public static function name($name)
+     {
+         $file = new File;
+         $file->_name = $name;
+
+         return $file;
+     }
+     
+     public final function setType($type)
+     {
+         $this->_type = $type;
+     }
+
+     public final function getPath()
+     {
+         if (!empty($this->_type))
+         {
+             return JSONDB_DATA_PATH.$this->_name.'.'.$this->_type.'.json';
+         }
+         else
+         {
+             throw new Exception('You must specify the type of file in class: '.__);
+         }
+     }
+
+     public final function get($assoc = false)
+     {
+         return json_decode(file_get_contents($this->getPath()), $assoc);
+     }
+
+     public final function put($data)
+     {
+         return file_put_contents($this->getPath(), json_encode($data));
+     }
+
+     public final function exists()
+     {
+         return file_exists($this->getPath());
+     }
+
+     public final function remove()
+     {
+         $type = ucfirst($this->_type);
+         if ($this->exists())
+         {
+             if (unlink($this->getPath()))
+                 return TRUE;
+
+             throw new Exception($type.': Deleting failed');
+         }
+
+         throw new Exception($type.': File does not exists');
+     }
+
+     /**
+      * Finds file from data folder
+      * @param array $names
+      * @param type $type
+      * @return boolean
+      */
+     public static function find_file(array $names, $type = 'config')
+     {
+         $names = implode(',', $names);
+         $pattern = './data/{'.$names.'}.'.$type.'.json';
+         $found = glob($pattern, GLOB_BRACE);
+         if (!empty($found))
+         {
+             preg_match('#./data/(.*).'.$type.'.json#', $found[0], $found_filename);
+             return $found_filename[1];
+         }
+
+         return false;
+     }
+
+ }
+
+?>
