5bb47c8d73cb214c7ae90554b6f83a59e8c6da09
Gregory <grego_92@o2.pl>
2013-11-24 01:11:00 +0100

    added files

---
+++ JSONDb/Classes/Helpers/Data.php
@@ -0,0 +1,28 @@
+<?php
+
+ namespace JSONDb\Classes\Helpers;
+
+defined('JSONDB_SECURE') or die('Permission denied!');
+
+ /**
+  * Data managing class
+  *
+  * @category Helpers
+  * @author Grzegorz Kuźnik
+  * @copyright (c) 2013, Grzegorz Kuźnik
+  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+  */
+ class Data extends File {
+
+     public static function name($name)
+     {
+         $file = new Data;
+         $file->_name = $name;
+         $file->setType('data');
+         
+         return $file;
+     }
+
+ }
+
+?>
