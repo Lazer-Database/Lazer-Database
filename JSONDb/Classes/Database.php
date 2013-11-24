b164f057b14446628b1ab99bf001c7e6e7570931
Gregory <grego_92@o2.pl>
2013-11-24 01:38:26 +0100

    new license

--- JSONDb/Classes/Database.php
+++ JSONDb/Classes/Database.php
@@ -4,18 +4,6 @@
 
 defined('JSONDB_SECURE') or die('Permission denied!');
 
- /**
-  * Extend of Core class of JSONDB project.
-  * 
-  * There are classes to use JSON files like file databes.
-  * 
-  * Using style was inspired by ORM classes.
-  *
-  * @category Core
-  * @author Grzegorz Kuźnik
-  * @copyright (c) 2013, Grzegorz Kuźnik
-  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
-  */
  class Database extends Core_Database { }
 
 ?>
